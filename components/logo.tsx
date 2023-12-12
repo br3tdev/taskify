fa98c39ef251378c2292a24f484081f6fb36b4d1
bret <bretai@LCARS-MX-23>
2023-12-12 15:36:04 +0300

    Shadcn config && Marketing Page

---
+++ components/logo.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export interface ILogoProps {}
+
+export default function Logo(props: ILogoProps) {
+  return (
+    <Link href="/">
+      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+}
