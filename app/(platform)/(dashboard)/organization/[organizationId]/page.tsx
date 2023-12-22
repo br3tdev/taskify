401342d942a0a935b7e2cda61a2233e35c69d640
bret <bretai@LCARS-MX-23>
2023-12-22 16:48:31 +0300

    useAction abstraction

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,23 @@
+import { db } from "@/lib/db";
+import Board from "./board";
+import Form from "./form";
+
 export interface IOrganizationIdPageProps {}
 
-export default function OrganizationIdPage(props: IOrganizationIdPageProps) {
+export default async function OrganizationIdPage(
+  props: IOrganizationIdPageProps,
+) {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      <h1>Organization Page</h1>
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          // <div key={board.id}>Board title: {board.title}</div>
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
     </div>
   );
 }
