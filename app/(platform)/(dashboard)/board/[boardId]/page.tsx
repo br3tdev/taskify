3e905ee87e4b3ce056c372be9290dc416fbc0f67
bret <bretai@LCARS-MX-23>
2023-12-28 14:04:04 +0300

    page metadata

---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,9 @@
+export interface IBoardIdPageProps {}
+
+export default function BoardIdPage(props: IBoardIdPageProps) {
+  return (
+    <div>
+      <h1>Board</h1>
+    </div>
+  );
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,14 @@
+import { auth } from "@clerk/nextjs";
 import OrgControl from "./_components/org-control";
+import { startCase } from "lodash";
+
+export function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 export default function OrganizationIdLayout({
   children,
